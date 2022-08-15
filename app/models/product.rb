300c6b9c2e697f985193f861c9f448a019960e73
Muhammad Haseeb <haseeb.nazeer@devsinc.com>
2022-08-15 12:44:02 +0500

    adding validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, :price, :description, :img_url, presense: true
+  validates :price, numericality: true
+  validates :description, length: { minimun: 5 }
 end
