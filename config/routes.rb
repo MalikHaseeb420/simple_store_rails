4b75b97b44131963a46ac1ce26fc65637c2aa07c
Muhammad Haseeb <haseeb.nazeer@devsinc.com>
2022-08-15 15:52:13 +0500

    more on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'product#index'
   get 'products', to: 'product#index', as: 'products'
-  get 'products/:id', to: 'product#show', as: 'product'
+  get 'products/:id', to: 'product#show', as: 'product', id: /\d+/
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
